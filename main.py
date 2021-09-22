e910e4eeb3b6eda3e0a3d114a5d0b907323855ff
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2021-09-22 20:52:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from replit import clear
+from art import logo
+#HINT: You can call clear() to clear the output in the console.
+
+def add_bidder():
+    auction_start = True
+
+    bidder = input("What is your name?\n> ")
+    
+    bid = int(input(f"Hi {bidder}, how much do you want to spend?\n> "))
+
+    return bidder_dict
+
+print(logo)
+
+player = add_bidder()
+
+print(auction_winner(player))
