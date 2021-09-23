83c3b1970102ed092d7516a50c005061a741097c
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2021-09-23 04:53:07 +0000

    - Updated Game Process - Refresh function for title - function for highest bid - Rename feature (prevents overwriting existing Dictionary entries in case of same names.

--- main.py
+++ main.py
@@ -2,17 +2,63 @@ from replit import clear
 from art import logo
 #HINT: You can call clear() to clear the output in the console.
 
-def add_bidder():
-    auction_start = True
+def refresh():
+    clear()
+    print(logo)
 
-    bidder = input("What is your name?\n> ")
-    
-    bid = int(input(f"Hi {bidder}, how much do you want to spend?\n> "))
+def add_bidder():
+    add_number = 1
+    bidder_dict = {}
+    auction_start = True
+    while auction_start:
+        refresh()
+        bidder = input("What is your name?\n> ")
+        bidding = True
+        refresh()
+        while bidding:
+            try:
+                bid = int(
+                input("How much do you want to spend?\nYour bid: €"))
+                refresh()
+            except:
+                print("You need a number to proceed")
+                refresh()
+                continue
+            
+            check = input(
+             f"Really go with €{bid}?\nPress 'N' to revise or any other key to confirm\n > ").lower()
+            if check == "n":
+                bidding = True
+            else:
+                bidding = False
+        
+        if bidder in bidder_dict:
+            add_number += 1
+            bidder = bidder + "-" + str(add_number)
+        bidder_dict[bidder] = bid
+        refresh()
+        check = input(
+         f"Add another bidder?\nPress 'Y' to add or any key to continue\n > ").lower()
+        if check == "y":
+            auction_start = True
+        else:
+            auction_start = False
 
     return bidder_dict
 
-print(logo)
+def auction_winner(auction_list):
+    winner = ""
+    highest_bid = 0
 
+    for bidder in auction_list:        
+       if auction_list[bidder] > highest_bid:
+          highest_bid = auction_list[bidder]
+          winner = bidder
+    refresh()
+    print(
+     f"The winner of this auction is {winner} with €{highest_bid}")
+
+refresh()
 player = add_bidder()
-
-print(auction_winner(player))
+refresh()
+auction_winner(player)
